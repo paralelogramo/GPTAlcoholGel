0b8c402d46e07b4ef3cfa2d11d91f60455225b29
Vicente Rojas <virojas15@alumnos.utalca.cl>
2022-05-17 23:44:43 -0400

    API changes added

--- src/app/API/db.php
+++ src/app/API/db.php
@@ -1,9 +1,9 @@
 <?php
 $password = "";
-$user = "";
-$db = "";
+$user = "root";
+$db = "gptalcoholgel";
 try {
-    return new PDO("mysql:host=localhost;dbname" . $db, $user, $password);
+    return new PDO("mysql:host=localhost;dbname=" . $db, $user, $password);
 } catch (Exception $e) {
     echo "Problemas: " . $e->getMessage();
 }
